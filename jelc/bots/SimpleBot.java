af369fe2c11ba992b4b66b14dbcd97a4ed8d826b
frak <frak>
2005-03-08 09:23:44 +0000

    lets get this organized

---
+++ jelc/bots/SimpleBot.java
@@ -0,0 +1,55 @@
+/*
+ * Created on 9/02/2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package bots;
+
+import elc.Client;
+
+
+/**
+ * @author dns
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class SimpleBot extends Client {
+	SimpleBot(String user, String password){
+		super(user, password);
+	}
+	SimpleBot(String user, String pass, String server, int port) {
+        super(user, pass, server, port);
+    }
+	/**
+	 * just log the console output like normal
+	 */
+	public void onChat(String message){
+		System.out.println(message);
+	}
+
+	/**
+	 * when a local message with the text 'hi' is recieved, reply with 'hi USER'
+	 */
+	public void onChat(String user, String message){
+		if(message.equalsIgnoreCase("hi")){
+			this.chat("hi "+user);
+		}
+	}
+
+
+	public static void main(String[] args) {
+		SimpleBot c;
+    	if(args.length < 2){
+    		System.out.println("usage: java elc/SimpleBot user pass [server port]");
+    		System.exit(1);
+    	}
+    	if(args.length == 4){
+    		c = new SimpleBot(args[0], args[1], args[2], Integer.valueOf(args[3]).intValue());
+    	} else {
+    		c = new SimpleBot(args[0], args[1]);	
+    	}
+        c.start();
+	}
+}

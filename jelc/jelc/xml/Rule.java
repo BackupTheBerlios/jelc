efb4308ea14a3ac7b7cfd7db6919f64a4c13f6b3
placid <placid>
2005-12-24 15:16:35 +0000

    Parsing of rules xml

---
+++ jelc/jelc/xml/Rule.java
@@ -0,0 +1,22 @@
+package jelc.xml;
+
+public class Rule {
+	private String shortRule;
+	private String longRule;
+	
+	public String getLongRule() {
+		return longRule;
+	}
+	
+	public void setLongRule(String longRule) {
+		this.longRule = longRule;
+	}
+	
+	public String getShortRule() {
+		return shortRule;
+	}
+	
+	public void setShortRule(String shortRule) {
+		this.shortRule = shortRule;
+	}
+}
